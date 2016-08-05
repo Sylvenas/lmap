df5a20ea4ea7fe681446b8a4a8c018294daf9355
sylvenas <sylvenas@outlook.com>
2016-08-05 20:06:04 +0800

    修改rbox动画效果

--- src/components/rbox/Rbox.js
+++ src/components/rbox/Rbox.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as action from '../../actions/searchAction';
-import styles from './_rbox.css'
+import styles from './_rbox.css';
 
 class Rbox extends React.Component {
+    constructor() {
+        super();
+        this.state={
+            contraction:false
+        }
+    }
+    contractionBtnClick(){
+        this.setState({
+            contraction:!this.state.contraction
+        })
+    }
     render() {
         console.log('i am the rbox');
         let searchAvtive = (this.props.searchValue === "");
         console.log(searchAvtive);
+        console.log('i am the contraction');
+        console.log(this.state.contraction);
         return (
             <div id="rbox" className={styles.rbox}>
                 {/*<div className={searchAvtive ? styles.rboxSearchPanCtrl_none : styles.rboxSearchPanCtrl_display}>展开指数面板</div>*/}
-                <div id="navBody">
+                <div id="navBody" className={this.state.contraction?styles.navBody_none:styles.navBody_display}>
                     <section id="rboxPanels" className={styles.rboxPanels}>
                         <ul id='nav' className={styles.nav}>
                             <li id='cross' className={styles.craLi}>
@@ -25,10 +38,11 @@ class Rbox extends React.Component {
                             </li>
                         </ul>
                         <div className={styles.resultPanel}>
+                            <h1>hello world</h1>
                         </div>
                     </section>
                 </div>
-                <div id="contractionBtn" className={styles.rboxPanCtrl}>
+                <div id="contractionBtn" className={styles.rboxPanCtrl} onClick={() =>this.contractionBtnClick()}>
                     <i className={styles.fa + ' ' + styles.faChevronUp} id="contractionInsideBtnUp"></i>
                 </div>
             </div>
